aac634d4e754ccd0243d716957e769abec09143b
csula-cs-496 <csula.jpl@gmail.com>
2014-10-06 17:23:34 -0700

    new version of helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,9 @@
 
 
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        System.out.println("Hello, World");
-    }
-
+class HelloWorld
+{
+   public static void main(String args[])
+   {
+      System.out.println("Hello World");
+   }
 }
